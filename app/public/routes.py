629032b6da87f921c5420d0d434a2130fd798d6b
gavinfriedrice <gabriela.kandeva5@gmail.com>
2023-07-08 12:05:00 +0200

    i think we're done

--- app/public/routes.py
+++ app/public/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from . import public
+from config.db.connection import Mongo
+from datetime import datetime
 
 from flask_breadcrumbs import register_breadcrumb,default_breadcrumb_root
 # Breadcrumbs load
@@ -8,5 +10,18 @@ default_breadcrumb_root(public, '.')
 @public.route('/', methods=['GET'])
 @register_breadcrumb(public, '.', 'Home')
 def index():
+    person_id = '0001'
+    my_application_data = {
+        'person_id': int(person_id),
+        'name': 'Gabriela',
+        'surname': 'Kandeva',
+        'created': datetime.now()
+    }
+
+    if Mongo.db.users.insert_one(my_application_data):
+        print("All was ok")
+    else:
+        print("something was wrong")
+
     return render_template('index.html')
 
